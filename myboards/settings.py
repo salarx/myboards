9d721d488a30cc5270fbe7e424a57c7eb9212705
Sahil Kumar <2017222@iiitdmj.ac.in>
2020-03-22 14:51:31 +0000

    updated authentication

--- myboards/settings.py
+++ myboards/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'myboards.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -175,7 +175,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
